b511e1cc495534739ab1e4544fe3a6206ed2623e
phil294 <philip@waritschlager.de>
2021-01-29 00:42:26 +0100

    Merge upstream /b/MEVN-base into master

--- web/src/plugins/http-client.js
+++ web/src/plugins/http-client.js
@@ -11,6 +11,9 @@ export default {
 			const token = store.state.session.token
 			if(token)
 				config.headers.Authorization = `Bearer ${token}`
+			/* TODO: Remove setting error_content and its references once https://github.com/axios/axios/issues/2387 is fixed: */
+			// @ts-ignore
+			// config.error_context = new Error("Thrown at:")
 			return config
 		})
 
@@ -23,6 +26,8 @@ export default {
 					data.http_query_url = http_query_url;
 				else
 					data += ` for ${http_query_url}`
+				// Can use this for better stack trace:
+				// axios_error.config?.error_context?.stack
 				return error(data, resp.status)
 			} else {
 				return error({ status: 0 }, 0)
